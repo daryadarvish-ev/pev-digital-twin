1ae86e0ff86aa613a6318069a306d9a24e9b81c6
Darya Darvish <daryadarvish@Daryas-Air.attlocal.net>
2022-10-24 22:20:16 -0700

    adding input data visualization

--- src/viz.py
+++ src/viz.py
@@ -1,3 +1,6 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 # Text wrap for nicer graph output
 import textwrap
 def wrap_labels(ax, width, break_long_words=False):
@@ -7,3 +10,12 @@ def wrap_labels(ax, width, break_long_words=False):
         labels.append(textwrap.fill(text, width=width,
                       break_long_words=break_long_words))
     ax.set_xticklabels(labels, rotation=0)
+    
+def plot_choice_per_vehicle_model(df):
+    # plot out top 10 vehicle brands at charging station
+    sns.histplot(df, x='choice', hue='vehicle_model', hue_order=df.vehicle_model.value_counts().iloc[:10].index, multiple='dodge')
+    
+def plot_correlation(df):
+    # plot correlation matrix of dataframe
+    plt.figure(figsize=(16, 10))
+    sns.heatmap(session_data.corr(), annot=True)
