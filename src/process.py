8ca9ad4b875e6487a5e170879c64446894dd4f62
Darya Darvish <daryadarvish@Daryas-Air.attlocal.net>
2022-10-31 21:43:18 -0700

    initial commit for computing metrics

---
+++ src/process.py
@@ -0,0 +1,16 @@
+
+
+def compute_metrics(df):
+    """Computer metrics such as
+        Slack
+
+        Distributions of Arrival & Departure
+
+        Distributions of Energy Delivered
+
+        Station-level power demand over time
+
+        Economics: Revenue, Utility Cost, etc
+
+        Choice: Split between REG and SCH
+        """
