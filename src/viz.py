fc0db97bbc750f2b8c9856ed2b86148da8cc9f8f
Joey Cai <caizhiyu84@gmail.com>
2022-11-02 08:37:40 +0800

    New plots illustrate relations between energy delivered and some other features.

--- src/viz.py
+++ src/viz.py
@@ -1,5 +1,16 @@
 # Text wrap for nicer graph output
 import textwrap
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly
+import plotly.graph_objects as go
+import plotly.io as pio
+from plotly.subplots import make_subplots
+from mpl_toolkits.mplot3d import Axes3D
+from pylab import *
+
 def wrap_labels(ax, width, break_long_words=False):
     labels = []
     for label in ax.get_xticklabels():
@@ -7,3 +18,31 @@ def wrap_labels(ax, width, break_long_words=False):
         labels.append(textwrap.fill(text, width=width,
                       break_long_words=break_long_words))
     ax.set_xticklabels(labels, rotation=0)
+
+def energyDdelivered(df):
+    """
+    Visualize the relations between delivered energy (column name: cumEnergy_Wh) and several other key metrics
+    df: Pandas Dataframe
+    """
+    pass
+
+
+def draw_barchart(x, y):
+    pass
+
+
+def draw_boxplot(x, y):
+    """
+    Draw a box plot.
+    x: Values
+    y: Labels
+    """
+    pass
+
+
+
+
+
+# df1 = pd.read_csv("../data/Sessions2_20221020.csv")
+# energyDdelivered(df1)
+
