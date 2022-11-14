166fe3f608a231032d3d99ab99048b163836b8ae
daryadarvish-ev <darya_darvish@berkeley.edu>
2022-11-13 17:34:38 -0800

    adding more visualization functions

--- src/viz.py
+++ src/viz.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 # Text wrap for nicer graph output
 import textwrap
+
+
 def wrap_labels(ax, width, break_long_words=False):
     labels = []
     for label in ax.get_xticklabels():
@@ -10,12 +12,27 @@ def wrap_labels(ax, width, break_long_words=False):
         labels.append(textwrap.fill(text, width=width,
                       break_long_words=break_long_words))
     ax.set_xticklabels(labels, rotation=0)
-    
-def plot_choice_per_vehicle_model(df):
-    # plot out top 10 vehicle brands at charging station
-    sns.histplot(df, hue='vehicle_model', hue_order=df.vehicle_model.value_counts().iloc[:10].index, multiple='dodge')
-    
+
+
+def plot_choice_per_vehicle_model(df, vehicle_models=['Model 3', 'Prius Prime', 'Volt', 'Bolt EV']):
+    # plot vehicle brand count at charging station per vehicle model
+    plt.figure(figsize=(20, 10))
+    sns.histplot(df[df.vehicle_model.isin(vehicle_models)], x='choice', hue='vehicle_model', multiple='dodge')
+
+
+def plot_choice_per_site(df):
+    # plot out choice selection count per site
+    plt.figure(figsize=(20, 10))
+    sns.histplot(df, x='choice', hue='siteId', multiple='dodge')
+
+
+def plot_cumulative_energy(df):
+    # plot cumulative energy histogram per choice
+    plt.figure(figsize=(20, 10))
+    sns.histplot(df, x='cumEnergy_Wh', hue='choice', multiple='dodge')
+
+
 def plot_correlation(df):
     # plot correlation matrix of dataframe
     plt.figure(figsize=(16, 10))
-    sns.heatmap(session_data.corr(), annot=True)
+    sns.heatmap(df.corr(), annot=True)
