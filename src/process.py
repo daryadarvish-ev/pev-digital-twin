b9c469b61995db849e0f9cb4383da9d211c01e85
daryadarvish-ev <darya_darvish@berkeley.edu>
2022-11-14 20:55:32 -0800

    Adding net income calculation

--- src/process.py
+++ src/process.py
@@ -30,6 +30,104 @@ def compute_slack(input_df):
     return input_df
 
 
+def compute_utility_cost(input_df):
+    """Compute utility cost and add a column to the dataframe for the utility cost per session
+
+    To calculate utility cost first unpack the 'power' column into a list with
+    power in Wh and timestamp which marks the start of a 5 min charging period.
+    The add up all the utility cost per session use the following equation:
+
+        utility_cost (cents) = TOU_COST (cents/kwh) * power (kw) * 5/60 hr
+
+    Parameters
+    ----------
+    input_df : pandas dataframe
+        input dataframe containing historical charging data
+
+    Returns
+    -------
+    output_df : pandas dataframe
+        pandas dataframe containing input data with column added for utility cost per session
+
+    """
+
+    # off-peak 0.175  cents / kwh
+    TOU_OFF_PEAK = 17.5
+    # 4 pm - 9 pm peak 0.367 cents / kwh
+    TOU_4PM_9PM = 36.7
+    # 9 am - 2 pm super off-peak 0.49 $ / kWh  to cents / kwh
+    TOU_9AM_2PM = 14.9
+
+    def get_utility_cost(row):
+        unpacked_power_list = unpack_power(row['power'])
+
+        total_utility_cost = 0
+
+        for power_kw, timestamp in unpacked_power_list:
+            if 16 < timestamp.hour < 21:
+                total_utility_cost += TOU_OFF_PEAK * power_kw * 5 / 60  # multiply by # hours
+            elif 9 < timestamp.hour < 14:
+                total_utility_cost += TOU_4PM_9PM * power_kw * 5 / 60
+            else:
+                total_utility_cost += TOU_9AM_2PM * power_kw * 5 / 60
+        return total_utility_cost/100
+
+    input_df['utility_cost'] = input_df.apply(lambda row: get_utility_cost(row), axis=1)
+    return input_df
+
+
+def unpack_power(power_row):
+    """Unpacks power column into list containing tuple of (kW, timestamp) for a charging session
+
+    Parameters
+    ----------
+    power_row :
+        row containing power data in string format
+
+    Returns
+    -------
+    unpacked_power_list : list
+        list of power in kW and timestamp
+
+    """
+
+    power_list = power_row.strip('][').split(', ')
+    unpacked_power_list = []
+
+    for i in range(0, len(power_list) // 2):
+        try:
+            power_kw = int(power_list[2 * i][21:25]) / 1000
+        except:
+            continue
+        timestamp = datetime.fromtimestamp(int(power_list[2 * i + 1][22:32]))
+        unpacked_power_list.append((power_kw, timestamp))
+    return unpacked_power_list
+
+
+def compute_net_income_per_session(input_df):
+    """
+    Calculates net income per sessions by subtracting estCost with utility cost
+
+    Parameters
+    ----------
+    power_row :
+        row containing power data in string format
+
+    Returns
+    -------
+    unpacked_power_list : list
+        list of power in kW and timestamp
+
+    """
+
+    ## todo sometimes the estCost column is NaN, figure out what to do in that case
+    try:
+        input_df['net_income'] = input_df['estCost'] - input_df['utility_cost']
+    except KeyError:
+        print('utility_cost column does not exist, try calling "compute_utility_cost" first')
+    return input_df
+
+
 def compute_revenue(input_df, interval='week'):
     """Computes revenue from the input dataframe
 
@@ -62,71 +160,35 @@ def compute_revenue(input_df, interval='week'):
     return revenue_df
 
 
-def compute_utility_cost(input_df):
-    """Compute utility cost and add a column to the dataframe for the utility cost per session
+def compute_net_income(input_df, interval='week'):
+    """Computes net income from the input dataframe
 
-    Utility cost calculation: cumEnergy_Wh/avg(Time-of-use)
+    Revenue can be computed per day, week, month, or year as specified, default is weekly.
+    Uses connectTime as aggregation column.
 
     Parameters
     ----------
     input_df : pandas dataframe
         input dataframe containing historical charging data
+    interval : str
+        'day', 'week', 'month', 'year'
 
     Returns
     -------
     output_df : pandas dataframe
-        pandas dataframe containing input data with column added for utility cost per session
-
+        pandas dataframe containing revenue per interval
     """
-
-    # off-peak 0.175  cents / kwh
-    TOU_OFF_PEAK = 17.5
-    ## 4 pm - 9 pm peak 0.367 cents / kwh
-    TOU_4PM_9PM = 36.7
-    ## 9 am - 2 pm super off-peak 0.49 $ / kWh  to cents / kwh
-    TOU_9AM_2PM = 14.9
-
-    def get_utility_cost(row):
-        unpacked_power_list = unpack_power(row['power'])
-
-        total_utility_cost = 0
-
-        for power_kw, timestamp in unpacked_power_list:
-            if 16 < timestamp.hour < 21:
-                total_utility_cost += TOU_OFF_PEAK * power_kw * 5 / 60  # multiply by # hours
-            elif 9 < timestamp.hour < 14:
-                total_utility_cost += TOU_4PM_9PM * power_kw * 5 / 60
-            else:
-                total_utility_cost += TOU_9AM_2PM * power_kw * 5 / 60
-        return total_utility_cost
-
-    input_df['utility_cost'] = input_df.apply(lambda row: get_utility_cost(row), axis=1)
-    return input_df
+    input_df['connectTime'] = pd.to_datetime(input_df['connectTime'])
+    if interval == 'day':
+        net_revenue_df = input_df.groupby(pd.Grouper(key='connectTime', freq='D'))['net_income'].sum()
+    elif interval == 'week':
+        net_revenue_df = input_df.groupby(pd.Grouper(key='connectTime', freq='W'))['net_income'].sum()
+    elif interval == 'month':
+        net_revenue_df = input_df.groupby(pd.Grouper(key='connectTime', freq='M'))['net_income'].sum()
+    elif interval == 'year':
+        net_revenue_df = input_df.groupby(pd.Grouper(key='connectTime', freq='Y'))['net_income'].sum()
+    else:
+        raise InvervalError
+    return net_revenue_df
 
 
-def unpack_power(power_row):
-    """Unpacks power column into list containing tuple of (kW, timestamp) for a charging session
-
-    Parameters
-    ----------
-    power_row :
-        row containing power data in string format
-
-    Returns
-    -------
-    unpacked_power_list : list
-        list of power in kW and timestamp
-
-    """
-
-    power_list = power_row.strip('][').split(', ')
-    unpacked_power_list = []
-
-    for i in range(0, len(power_list) // 2):
-        try:
-            power_kw = int(power_list[2 * i][21:25]) / 1000
-        except:
-            continue
-        timestamp = datetime.fromtimestamp(int(power_list[2 * i + 1][22:32]))
-        unpacked_power_list.append((power_kw, timestamp))
-    return unpacked_power_list
