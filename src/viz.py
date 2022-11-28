8011ef077aee3d2574691ae4e28d18fbb0900aaa
daryadarvish-ev <114886098+daryadarvish-ev@users.noreply.github.com>
2022-11-27 17:32:48 -0800

    Revert "Revert "Revert "Add some more visualization functions"""

--- src/viz.py
+++ src/viz.py
@@ -1,6 +1,3 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 # Text wrap for nicer graph output
 import textwrap
 from process import unpack_power
@@ -13,30 +10,6 @@ def wrap_labels(ax, width, break_long_words=False):
         labels.append(textwrap.fill(text, width=width,
                       break_long_words=break_long_words))
     ax.set_xticklabels(labels, rotation=0)
-
-
-def plot_choice_per_vehicle_model(df, vehicle_models=['Model 3', 'Prius Prime', 'Volt', 'Bolt EV']):
-    # plot vehicle brand count at charging station per vehicle model
-    plt.figure(figsize=(20, 10))
-    sns.histplot(df[df.vehicle_model.isin(vehicle_models)], x='choice', hue='vehicle_model', multiple='dodge')
-
-
-def plot_choice_per_site(df):
-    # plot out choice selection count per site
-    plt.figure(figsize=(20, 10))
-    sns.histplot(df, x='choice', hue='siteId', multiple='dodge')
-
-
-def plot_cumulative_energy(df):
-    # plot cumulative energy histogram per choice
-    plt.figure(figsize=(20, 10))
-    sns.histplot(df, x='cumEnergy_Wh', hue='choice', multiple='dodge')
-
-
-def plot_correlation(df):
-    # plot correlation matrix of dataframe
-    plt.figure(figsize=(16, 10))
-    sns.heatmap(df.corr(), annot=True)
 
 
 def viz_charging_profile(power_row):
