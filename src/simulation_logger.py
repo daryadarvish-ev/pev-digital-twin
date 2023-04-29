375a0762438c5b470d3cc5fcfff1b6d1c0e19554
daryadarvish-ev <darya_darvish@berkeley.edu>
2023-04-29 15:42:29 -0700

    lastest logger with carbon emissions

--- src/simulation_logger.py
+++ src/simulation_logger.py
@@ -1,47 +1,27 @@
 import math
 import datetime
 
-import pandas as pd
 from carbon_emissions import *
 
 
 class SimulationLogger:
     def __init__(self):
-        self.user_data = pd.DataFrame(columns=['e_need', 'ASAP_Power', 'FLEX_power', 'flex_tarrif', 'asap_tarrif',
+        self.user_data = pd.DataFrame(columns=['arrival_time', 'e_need', 'ASAP_Power', 'FLEX_power', 'flex_tarrif', 'asap_tarrif',
                                                'choice', 'station_power_profile'])
-        self.station_power = pd.DataFrame(columns=['station_power'])
-        self.aggregate_metrics = pd.DataFrame(columns=['isotime', 'utility cost','carbon_emissions', 'revenue'])
+        self.station_power = pd.DataFrame(columns=['time', 'station_power'])
+        self.aggregate_metrics = pd.DataFrame(columns=['isotime', 'utility cost', 'carbon_emissions', 'revenue'])
+        self.user_count = 0
 
     def add_data(self, time, res):
+        self.user_count += 1
         station_power_profile = res["sch_agg"] if res['choice'] == 'Scheduled' else res['reg_agg']
-        self.user_data.loc[time] = [res['e_need'], res['asap_powers'], res['flex_powers'], res['tariff_flex'], res['tariff_asap'], res['choice'], station_power_profile]
-        return self.user_data
-
-    def get_eneed(self, time):
-        e_need = self.user_data.loc[time, 'e_need']
-        return e_need
-
-    def get_asap_power(self, time):
-        asap_power = self.user_data.loc[time, 'ASAP_Power']
-        return asap_power
-
-    def get_flex_power(self, time):
-        flex_power = self.user_data.loc[time, 'FLEX_power']
-        return flex_power
-
-    def get_tariff_flex(self, time):
-        tariff_flex = self.user_data.loc[time, 'flex_tarrif']
-        return tariff_flex
-
-    def get_tariff_asap(self, time):
-        tariff_asap = self.user_data.loc[time, 'asap_tarrif']
-        return tariff_asap
+        self.user_data.loc[self.user_count] = [time, res['e_need'], res['asap_powers'], res['flex_powers'], res['tariff_flex'], res['tariff_asap'], res['choice'], station_power_profile]
 
     def process_station_power(self):
         for index, row in self.user_data.iterrows():
             if row['choice'] != 'Leave':
                 for timestep in range(len(row['station_power_profile'])):
-                    self.station_power.loc[int(index) + 15*timestep] = row['station_power_profile'][timestep]
+                    self.station_power.loc[index] = [row['arrival_time'] + 15*timestep, row['station_power_profile'][timestep]]
 
     def compute_aggregate_metrics(self):
         """Computes station carbon emissions, utility cost, and revenue.
@@ -72,8 +52,8 @@ class SimulationLogger:
         emissions_df = get_emissions(token, starttime, endtime)
 
         for index, row in self.station_power.iterrows():
-            hour_of_day = (int(index)%1440)/60
-            index_datetime = starttime_datetime + datetime.timedelta(minutes=int(index))
+            hour_of_day = (row['time']%1440)/60
+            index_datetime = starttime_datetime + datetime.timedelta(minutes=row['time'])
             endtime_datetime = index_datetime + datetime.timedelta(minutes=15)
 
             # C02 Emissions (lbs) = power_kw * hr * 0.001mw/1kw *lbs/mwh
@@ -81,18 +61,18 @@ class SimulationLogger:
                                             (emissions_df['point_time'] <= endtime_datetime.isoformat())]['value'].sum() * row['station_power'] * (15/60) * 0.001
 
             if 16 < hour_of_day < 21:
-                self.aggregate_metrics.loc[int(index)] = [index_datetime, TOU_OFF_PEAK * row['station_power'] * 15 / (60*100), carbon_emissions, 0]
+                self.aggregate_metrics.loc[index] = [index_datetime, TOU_OFF_PEAK * row['station_power'] * 15 / (60*100), carbon_emissions, 0]
             elif 9 < hour_of_day < 14:
-                self.aggregate_metrics.loc[int(index)] = [index_datetime, TOU_4PM_9PM * row['station_power'] * 15 / (60*100), carbon_emissions, 0]
+                self.aggregate_metrics.loc[index] = [index_datetime, TOU_4PM_9PM * row['station_power'] * 15 / (60*100), carbon_emissions, 0]
             else:
-                self.aggregate_metrics.loc[int(index)] = [index_datetime, TOU_9AM_2PM * row['station_power'] * 15 / (60*100), carbon_emissions, 0]
+                self.aggregate_metrics.loc[index] = [index_datetime, TOU_9AM_2PM * row['station_power'] * 15 / (60*100), carbon_emissions, 0]
 
         # get revenue per user
         for index, row in self.user_data.iterrows():
             choice = row['choice']
             if choice == 'Scheduled':
-                self.aggregate_metrics.at[int(index), 'revenue'] = row['flex_tarrif']
+                self.aggregate_metrics.at[index, 'revenue'] = row['flex_tarrif']
             elif choice == 'Regular':
-                self.aggregate_metrics.at[int(index), 'revenue'] = row['asap_tarrif']
+                self.aggregate_metrics.at[index, 'revenue'] = row['asap_tarrif']
             else:
-                self.aggregate_metrics.at[int(index), 'revenue'] = 0
+                self.aggregate_metrics.at[index, 'revenue'] = 0
